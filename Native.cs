fa2f1a222f60787a77b7fd87ac2c670ed19fd3cd
xyx0826 <xyx0826@hotmail.com>
2020-04-26 20:43:07 -0700

    Cleanup

--- Native.cs
+++ Native.cs
@@ -3,33 +3,33 @@ using System.Runtime.InteropServices;
 
 namespace RevorbStd
 {
-    public class Native
+    public static class Native
     {
         [StructLayout(LayoutKind.Sequential)]
-        public struct REVORB_FILE
+        public struct RevorbFile
         {
-            public IntPtr start;
-            public IntPtr cursor;
-            public long size;
+            public IntPtr Start;
+            public IntPtr Cursor;
+            public long Size;
         }
 
-        public const int REVORB_ERR_SUCCESS = 0;
-        public const int REVORB_ERR_NOT_OGG = 1;
-        public const int REVORB_ERR_FIRST_PAGE = 2;
-        public const int REVORB_ERR_FIRST_PACKET = 3;
-        public const int REVORB_ERR_HEADER = 4;
-        public const int REVORB_ERR_TRUNCATED = 5;
-        public const int REVORB_ERR_SECONDARY_HEADER = 6;
-        public const int REVORB_ERR_HEADER_WRITE = 7;
-        public const int REVORB_ERR_CORRUPT = 8;
-        public const int REVORB_ERR_BITSTREAM_CORRUPT = 9;
-        public const int REVORB_ERR_WRITE_FAIL = 10;
-        public const int REVORB_ERR_WRITE_FAIL2 = 11;
+        public enum RevorbResult : int
+        {
+            Success,
+            NotOggError,
+            FirstPageError,
+            FirstPacketError,
+            HeaderError,
+            TruncatedError,
+            SecondaryHeaderError,
+            WriteHeaderError,
+            CorruptError,
+            BitstreamCorruptError,
+            WriteError,
+            EOSWriteError
+        }
 
         [DllImport("librevorb.dll", CallingConvention = CallingConvention.Cdecl)]
-        internal static extern int revorb(ref REVORB_FILE fi, ref REVORB_FILE fo);
-
-        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-        public delegate IntPtr ReAlloc(IntPtr pv, ulong cb);
+        internal static extern int revorb(ref RevorbFile fi, ref RevorbFile fo);
     }
 }
--- Revorb.cs
+++ Revorb.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using static RevorbStd.Native;
 
@@ -8,104 +7,70 @@ namespace RevorbStd
 {
     public static class Revorb
     {
-        public static Stream Jiggle(Stream inputFile)
+        /// <summary>
+        /// Revorbs Ogg Vorbis data from the input stream.
+        /// </summary>
+        /// <param name="inputFile">The Ogg Vorbis physical input stream.</param>
+        /// <returns>A <see cref="MemoryStream"/> containing revorbed data.</returns>
+        public static MemoryStream Jiggle(Stream inputFile)
         {
-            byte[] raw = new byte[inputFile.Length];
-            long pos = inputFile.Position;
-            inputFile.Position = 0;
-            inputFile.Read(raw, 0, raw.Length);
-            inputFile.Position = pos;
-            return Jiggle(raw);
+            if (inputFile is MemoryStream
+                && (inputFile as MemoryStream).TryGetBuffer(out var streamBuf))
+            {
+                // Use underlying stream buffer
+                return Jiggle(streamBuf.Array);
+            }
+            else
+            {
+                // Read stream into array, preserve position
+                var input = new byte[inputFile.Length];
+                var cursor = inputFile.Position;
+                inputFile.Position = 0;
+                inputFile.Read(input, 0, input.Length);
+                inputFile.Position = cursor;
+                return Jiggle(input);
+            }
         }
 
-        public static Stream Jiggle(byte[] input)
+        /// <summary>
+        /// Revorbs Ogg Vorbis data from the input buffer.
+        /// </summary>
+        /// <param name="input">The Ogg Vorbis physical stream data.</param>
+        /// <returns>A <see cref="MemoryStream"/> containing revorbed data.</returns>
+        public static MemoryStream Jiggle(byte[] input)
         {
+            // Pin input
             var hInput = GCHandle.Alloc(input, GCHandleType.Pinned);
             var pInput = hInput.AddrOfPinnedObject();
-            REVORB_FILE inputFile = new REVORB_FILE {
-                start = pInput,
-                cursor = pInput,
-                size = input.Length
+            RevorbFile inputFile = new RevorbFile {
+                Start = pInput,
+                Cursor = pInput,
+                Size = input.Length
             };
 
-            var output = new byte[input.Length + 4096];
+            // Create and pin output
+            var output = new byte[input.Length + 4096]; // Arbitrary extra space
             var hOutput = GCHandle.Alloc(output, GCHandleType.Pinned);
             var pOutput = hOutput.AddrOfPinnedObject();
-            REVORB_FILE outputFile = new REVORB_FILE
+            RevorbFile outputFile = new RevorbFile
             {
-                start = pOutput,
-                cursor = pOutput,
-                size = output.Length
+                Start = pOutput,
+                Cursor = pOutput,
+                Size = output.Length
             };
 
-            //int result = revorb(ref input, ref output, _reAlloc);
             int result = revorb(ref inputFile, ref outputFile);
 
+            // Unpin buffers
             hInput.Free();
             hOutput.Free();
-            if (result <= REVORB_ERR_WRITE_FAIL2 && result > REVORB_ERR_SUCCESS)
+
+            if (Enum.IsDefined(typeof(RevorbResult), result))
             {
-                throw new Exception($"Expected success, got {result} -- refer to RevorbStd.Native");
+                throw new Exception($"Revorb error {result} -- refer to RevorbStd.Native");
             }
 
             return new MemoryStream(output, 0, result);
-        }
-
-        public unsafe class RevorbStream : UnmanagedMemoryStream
-        {
-            private REVORB_FILE revorbFile;
-
-            public RevorbStream(REVORB_FILE revorbFile) : base((byte*)revorbFile.start.ToPointer(), revorbFile.size)
-            {
-                this.revorbFile = revorbFile;
-            }
-            
-            public new void Dispose()
-            {
-                base.Dispose();
-                Marshal.FreeHGlobal(revorbFile.start);
-            }
-        }
-
-        public static void Main(string[] args)
-        {
-            if (args.Contains("-t"))
-            {
-                Test(args[0]);
-                return;
-            }
-
-            try
-            {
-                using (Stream file = File.OpenRead(args[0]))
-                {
-                    using (Stream data = Jiggle(file))
-                    {
-                        using (Stream outp = File.OpenWrite(args[1]))
-                        {
-                            data.Position = 0;
-                            data.CopyTo(outp);
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine(e.ToString());
-            }
-        }
-
-        public static void Test(string path, int count = 10000, int outSize = 4096)
-        {
-            for (var i = 0; i < count; i++)
-            {
-                var raw = File.ReadAllBytes(path);
-                using (var output = Jiggle(raw))
-                {
-                    Console.WriteLine($"#{i + 1}: {output.Length}");
-                    output.Dispose();
-                }
-            }
         }
     }
 }
